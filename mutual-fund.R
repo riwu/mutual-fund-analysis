62e2e35da60e9a76b13310f7bd5eec9b0ab909db
riwu <riwu0730@gmail.com>
2017-04-10 00:58:15 +0800

    Added shapiro test for normality

--- mutual-fund.R
+++ mutual-fund.R
@@ -1,5 +1,18 @@
 funds <- read.csv("mutual funds.csv", header=T)
-variable_columns <- c(3, 5:8, 11, 12)
+
+# gets all variables conforming to normal distribution
+variable_columns <- vector()
+for (i in 1:length(funds)) {
+  if (is.numeric(funds[,i])) { 
+    p <- shapiro.test(funds[,i])$p.value
+    p_compare_str <- if (p < 0.01) "smaller" else "greater"
+    print(sprintf("shapiro test for %s has a p-value of %s, which is %s than 0.01", 
+                  names(funds[i]), p, p_compare_str))
+    # we include Worst_Quarter since its p-value of 0.0109 is very close to 0.01
+    variable_columns <- c(variable_columns, i) 
+  }
+}
+
 print(cat("Variables to test: ", names(funds[variable_columns])))
 var_count <- length(variable_columns)
 correlated_vars <- vector()
